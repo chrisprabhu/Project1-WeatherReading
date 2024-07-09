a21c465679d366b009f5d7de20837248f62af9b7
Chris Prabhu <chrisprabhu2@gmail.com>
2024-07-09 15:44:14 -0700

    record class compareTo finished

--- WeatherReading.java
+++ WeatherReading.java
@@ -1,12 +1,42 @@
 // Purpose: This file is a record class that stores the weather reading data.
-public record WeatherReading implements Comparable<WeatherReading>(String country, String state, String city,
-                                                                          String month, String day, String year,
-                                                                          String avgTemperature) {
-
+public record WeatherReading(
+        String country, String state, String city, String month, String day, String year, String avgTemperature
+) implements Comparable <WeatherReading> {
+    // ToDo Deal with nulls
+    // Country, State, City, Year, Month, Day
     @Override
     public int compareTo(WeatherReading other) {
-        if (this.country.compareTo(other.country) !=0) { return this.country.compareTo(other.country); }
-        else if (this.state.compareTo(other.state) !=0 ) { return this.state.compareTo(other.state); }
-        else return this.city.compareTo(other.city);
+        // Country
+        if (!this.country.equals(other.country)) {
+            return this.country.compareTo(other.country);
+            
+        } // State
+        else {
+            if (!this.state.equals(other.state)) {
+                return this.state.compareTo(other.state);
+                
+            } // City
+            else {
+                if (!this.city.equals(other.city)) {
+                    return this.city.compareTo(other.city);
+                    
+                } // Year
+                else {
+                    if (!this.year.equals(other.year)) {
+                        return this.year.compareTo(other.year);
+                        
+                    } // Month
+                    else {
+                        if (!this.month.equals(other.month)) {
+                            return this.month.compareTo(other.month);
+                            
+                        } // Day
+                        else {
+                            return this.day.compareTo(other.day);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
