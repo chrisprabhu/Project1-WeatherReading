e7f5b89edb3ac0ac70c3ce9740ce7fe2803ea2d0
Chris Prabhu <chrisprabhu2@gmail.com>
2024-07-09 13:06:18 -0700

    before tests

--- WeatherReading.java
+++ WeatherReading.java
@@ -1,2 +1,12 @@
 // Purpose: This file is a record class that stores the weather reading data.
-public record WeatherReading implements Comparable <WeatherReading> (String country,String state,String city,String month,String day,String year,String avgTemperature){}
+public record WeatherReading implements Comparable<WeatherReading>(String country, String state, String city,
+                                                                          String month, String day, String year,
+                                                                          String avgTemperature) {
+
+    @Override
+    public int compareTo(WeatherReading other) {
+        if (this.country.compareTo(other.country) !=0) { return this.country.compareTo(other.country); }
+        else if (this.state.compareTo(other.state) !=0 ) { return this.state.compareTo(other.state); }
+        else return this.city.compareTo(other.city);
+    }
+}
